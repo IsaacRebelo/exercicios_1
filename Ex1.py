73bc2eefe1fb27f76bd51f8ddcabeed98a2de523
Isaac <isaacaraujorebelo@enta.pt>
2022-09-26 17:48:53 +0000

    Resolução sem apresentar o número maior e menor

--- Ex1.py
+++ Ex1.py
@@ -8,3 +8,55 @@ Após o utilizador ter inserido os 5 valores apresente:
 - O maior valor inserido
 - A média das vendas
 """
+
+def menorvalor(numeros):
+
+    menorvalor_num = numeros[0]
+
+    for num in numeros:
+        if num < menorvalor_num:
+            menorvalor_num = num
+
+    return menorvalor_num
+
+
+def maiorvalor(numeros):
+
+    maiorvalor_num = numeros[0]
+
+    for num in numeros:
+        if num > maiorvalor_num:
+            maiorvalor_num = num
+
+    return maiorvalor_num
+
+Vendas= [0,0,0,0,0,0]
+ilhas = ["Terceira", "Sao Jorge", "Pico", "Faial", "Graciosa"]
+
+
+if __name__ == '__main__':
+    continuar = 's'
+    while continuar == 's':
+        ilha= input('Insira o nome da ilha')
+        pos = ilhas.index(ilha)
+        valor1 = int(input('Insire o 1valor'))
+        valor2 = int(input('Insire o 2valor'))
+        valor3 = int(input('Insire o 3valor'))
+        valor4 = int(input('Insire o 4valor'))
+        valor5 = int(input('Insire o 5valor'))
+        operação = "+"
+        total= valor1 + valor2 + valor3 + valor4 + valor5
+        average = total /5
+        print(f'average: {average}')
+        print(f"total : {total}")
+        print(f"menor: {menorvalor(Vendas)}")
+        print(f"maior: {maiorvalor(Vendas)}")
+
+
+
+
+
+
+
+
+
