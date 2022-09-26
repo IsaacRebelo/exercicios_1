e3e9a91ec59d36ac587e807f3047688f4ad7690a
Isaac <isaacaraujorebelo@enta.pt>
2022-09-26 20:20:57 +0000

    RESOLUÇÃO SEM  A FRASE INVERTIDA

--- Ex2.py
+++ Ex2.py
@@ -9,3 +9,38 @@ Após o utilizador ter inderido a frase apresente:
  - Quantos numeros tem a frase
  - Apresente a frase invertida. Exemplo: A frase é 'Bom dia!' deve dar '!aid moB'
 """
+str1 = input('Introduza a sua frase')
+vogais= 'aeiouAEIOU'
+conta_vogais= 0
+conta_consoantes= 0
+conta_tamanhodafrase=0
+l,u=0,0
+consoantes= 'bcdfghjklmnpqrstvwxzBCDFGHJKLMNPQRSTVWXZ'
+
+for i in str1:
+    if (i >='A' and i <='Z'):
+        l=l+1
+    if(i >='a' and i <='z'):
+        u=u+1
+
+for i in str1:
+    if i in vogais:
+        conta_vogais+=1
+    if i in consoantes:
+        conta_consoantes+=1
+for k in str1:
+    conta_tamanhodafrase=conta_tamanhodafrase+1
+
+(print(f'vogais: {conta_vogais}'))
+(print(f'consoantes:{conta_consoantes}'))
+(print(f'tamanho da frase: {conta_tamanhodafrase}'))
+(print(f'maiusculas e minusculas da frase: {l,u}'))
+
+
+
+
+
+
+
+
+
